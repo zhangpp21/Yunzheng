40ee24dd36128719579308d796fab2e04ed40c6c
sadalsuud <sadalsuudoff@outlook.com>
2024-02-23 23:57:52 +0800

    shiirui-mapperpart

--- src/main/java/com/ydsy/mapper/AnnouncementMapper.java
+++ src/main/java/com/ydsy/mapper/AnnouncementMapper.java
@@ -1,6 +1,13 @@
 package com.ydsy.mapper;
 
 
+import com.ydsy.pojo.Announcement;
+
 public interface AnnouncementMapper {
+    Announcement getAnnouncementById(int id);
+
+    void insertAnnouncement(Announcement announcement);
+
+    // 其他可能的方法，比如更新公告内容、删除公告等
 
 }
--- src/main/java/com/ydsy/mapper/MeetingMapper.java
+++ src/main/java/com/ydsy/mapper/MeetingMapper.java
@@ -1,6 +1,13 @@
 package com.ydsy.mapper;
 
 
+import com.ydsy.pojo.Meeting;
+
 public interface MeetingMapper {
+    Meeting getMeetingById(int id);
+
+    void insertMeeting(Meeting meeting);
+
+    // 其他可能的方法，比如更新会议内容、删除会议等
 
 }
