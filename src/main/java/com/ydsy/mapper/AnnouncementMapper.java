2daa3d9dc8a4c47ed94425e2787f46b8a67733da
sadalsuud <sadalsuudoff@outlook.com>
2024-03-06 22:00:51 +0800

    完善了一部分代码 更改了存在的错误 整理

--- src/main/java/com/ydsy/mapper/AnnouncementMapper.java
+++ src/main/java/com/ydsy/mapper/AnnouncementMapper.java
@@ -4,7 +4,7 @@ import com.ydsy.pojo.Announcement;
 import org.apache.ibatis.annotations.Insert;
 
 public interface AnnouncementMapper {
-    @Insert("INSERT INTO announcements (announcement_content, direction, period, user_id, created_at) " +
-            "VALUES (#{content}, #{direction}, #{period}, #{userId}, #{createdAt})")
+    @Insert("INSERT INTO announcements (announcement_content, direction, period, user_id, created_at,creator_id) " +
+            "VALUES (#{content}, #{direction}, #{period}, #{userId}, #{createdAt},#{creatorId})")
     void insertAnnouncement(Announcement announcement);
 }
